11a684f1e6b09ed1e5c16d9f26579872208b2c81
jaeyong <jessica9685@ewhain.net>
2020-05-01 22:03:39 +0900

    week3_jaeyong

---
+++ 공통문제/0501/2019_카카오_호텔_방_배정/2019_카카오_호텔_방_배정.cpp
@@ -0,0 +1,31 @@
+﻿// 2019_카카오_호텔_방_배정.cpp
+#include <iostream>
+#include <string>
+using namespace std;
+using namespace stdext;
+#include <vector>
+#include <algorithm>
+#include <hash_map>
+
+hash_map<long long, long long> rooms; // 초기값 = 0
+
+long long reservation(long long room_number) { // 방번호 배정
+	if (rooms[room_number] == 0) {
+		rooms[room_number] = 1;
+		return room_number;
+	}
+	else {
+		return room_number = reservation(room_number + 1);
+	}
+}
+
+vector<long long> solution(long long k, vector<long long> room_number) {
+	long long i;
+	vector<long long> answer;
+	
+	for (i = 0; i < room_number.size(); i++) {
+		answer.push_back(reservation(room_number[i]));
+	}
+	
+	return answer;
+}
