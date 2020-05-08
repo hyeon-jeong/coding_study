7d01ddce809717b38c9215dd5390c76369150f39
jystella17 <42269221+jystella17@users.noreply.github.com>
2020-05-08 18:33:02 +0900

    Create personal1.cpp

---
+++ 개인문제/정연/week4/personal1.cpp
@@ -0,0 +1,16 @@
+// Leetcode - Maximum 69 Number
+
+#include <bits/stdc++.h>
+
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string digit = to_string(num);
+        for (int i=0; i<digit.size(); i++){
+            if (digit[i] == '6'){
+                digit[i] = '9';
+                break;
+            }
+        } return stoi(digit);
+    }
+};
