01c0f92b9aea7a06a03ac68d5d96674f11712950
jessica9685 <49610085+jessica9685@users.noreply.github.com>
2020-07-04 16:00:47 +0900

    Add files via upload

---
+++ 공통문제/week_8/재용.cpp
@@ -0,0 +1,25 @@
+﻿// 9700_USB_꽃기의_미스터리.cpp
+#include <iostream>
+using namespace std;
+
+int T;
+double p, q;
+double s1, s2;
+
+int main()
+{
+	cin >> T;
+	for (int i = 1; i <= T; i++) {
+		cin >> p >> q;
+
+		s1 = (1 - p) * q;
+		s2 = p * (1 - q) * q;
+
+		if (s1 < s2) {
+			cout << "#" << i << " YES" << endl;
+		}
+		else {
+			cout << "#" << i << " NO" << endl;
+		}
+	}
+}
