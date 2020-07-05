454ac1d881f199a4b3c9dff25cde1f7fe7f992bf
jaeyong <jessica9685@ewhain.net>
2020-07-05 16:15:12 +0900

    week10

---
+++ 개인문제/재용/0703/1989_초심자의_회문검사/1989_초심자의_회문검사.cpp
@@ -0,0 +1,31 @@
+﻿// 1989_초심자의_회문검사.cpp
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+#include <stdio.h>
+#include <cstring>
+
+int main()
+{
+	int T;
+	scanf("%d", &T);
+
+	for (int tc = 1; tc <= T; tc++) {
+		char word[10];
+		scanf("%s", word);
+
+		int len = strlen(word);
+		int p = 1;
+		for (int i = 0; i < len / 2; i++) {
+			if (word[i] != word[len - i - 1]) {
+				p = 0;
+				break;
+			}
+			else continue;
+		}
+		printf("#%d", tc);
+		printf(" %d\n", p);
+	}
+	return 0;
+}
+
