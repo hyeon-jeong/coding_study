03728f3e767e2ce837424d4f3de99990dcc08d33
jystella17 <42269221+jystella17@users.noreply.github.com>
2020-07-05 23:10:44 +0900

    Create SWE1984.cpp

---
+++ 개인문제/정연/week9/SWE1984.cpp
@@ -0,0 +1,40 @@
+//중간 평균값 구하기
+
+#include<iostream>
+#include<stdio.h>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+bool sorting(const int &n1, const int &n2){
+    return n1 < n2;
+}
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+	scanf("%d", &T);
+    int num[10];
+    vector<int> v;
+    
+	for(test_case = 1; test_case <= T; ++test_case)
+	{
+        for(int i=0; i<10; i++){
+            scanf("%d", num[i]);
+            v.push_back(num[i]);
+        }
+        sort(v.begin(), v.end(), sorting);
+        v.erase(v.begin()); v.erase(v.end());
+        
+        int sum = 0;
+        for (auto& n : v){
+            sum += n;
+        }
+            
+        int avg = sum;
+        printf("#%d %d\n", test_case, avg);
+	}
+	return 0;
+}
