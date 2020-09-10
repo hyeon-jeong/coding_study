a99ae4cae48d77e0cc3f51dcb86e98e86a76f128
jystella17 <42269221+jystella17@users.noreply.github.com>
2020-09-11 02:13:09 +0900

    Create 영화감독 숌.cpp

---
+++ 개인문제/정연/week20/영화감독 숌.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int n,cnt,tmp,flag,ans;
+
+int main(){
+    cin >> n;
+
+    while(cnt != n){
+        ans++;
+        flag = 0;
+        tmp = ans;
+        while(tmp){
+            if (tmp % 1000 == 666)flag = 1; // 체크하는 숫자를 1000으로 나눴을 때의 나머지가 666 = 666을 포함하는 숫자
+            tmp /= 10; // 666으로 나누어떨어지지 않는 경우 ex. 106661 >> 10으로 나누면 10666.1이 되므로 다음번 loop에서 flag = 1이 됨
+        }
+        
+        if (flag){cnt++;}
+    }  
+    cout << ans << '\n';
+    return 0;
+}
