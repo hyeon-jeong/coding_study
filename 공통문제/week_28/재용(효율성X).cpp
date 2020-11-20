dbb4149856259c0f197872cf24d63fdd9d8cf6b6
jessica9685 <49610085+jessica9685@users.noreply.github.com>
2020-11-20 17:02:28 +0900

    Update 재용(효율성X).cpp

--- 공통문제/week_28/재용(효율성X).cpp
+++ 공통문제/week_28/재용(효율성X).cpp
@@ -7,14 +7,14 @@ using namespace std;
 vector<int> solution(vector<string> words, vector<string> queries) {
 	vector<int> answer;
 
-	for (int q = 0; q < queries.size(); q++) {
+	for (int q = 0; q < queries.size(); q++) { // 쿼리
 		string query = queries[q];
 
 		bool flag = true;
 		set<string> result;
-		for (int w = 0; w < words.size(); w++) {
+		for (int w = 0; w < words.size(); w++) { // 단어마다 쿼리 일치 여부 확인
 			string word = words[w];
-			if (word.length() != query.length()) continue;
+			if (word.length() != query.length()) continue; // 길이 다르면 넘어감
 			for (int i = 0; i < query.length(); i++) {
 				if (query[i] == '?') continue;
 				if (query[i] != word[i]) {
@@ -26,7 +26,7 @@ vector<int> solution(vector<string> words, vector<string> queries) {
 				flag = true;
 				continue;
 			}
-			else {
+			else { // 위의 모든 조건을 통과하면 result set에 추가
 				result.insert(word);
 			}
 		}
