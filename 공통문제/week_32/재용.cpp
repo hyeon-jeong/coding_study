b822a4b7feaed66be61b15bd2c5c9ffbca0b2e69
jessica9685 <49610085+jessica9685@users.noreply.github.com>
2020-12-26 22:18:58 +0900

    Update 재용.cpp

--- 공통문제/week_32/재용.cpp
+++ 공통문제/week_32/재용.cpp
@@ -14,7 +14,7 @@ vector<pair<int, int>> network[MAX];
 int dist[MAX];
 
 bool dijkstra(int mid) {
-	memset(dist, INF, sizeof(dist));
+	for (int i = 0; i <= N; i++) dist[i] = INF;
 
 	priority_queue<pair<int, int>> pq; // first: cost, second: next point
 	dist[1] = 0;
@@ -27,13 +27,13 @@ bool dijkstra(int mid) {
 		pq.pop();
 
 		if (dist[cur] < cur_dist) continue;
-		for (auto &p : network[cur]) {
+		for (auto p : network[cur]) {
 			int next = p.first;
 			int next_cost = cur_dist + (p.second > mid); // mid보다 가중치가 크면 1, 작으면 0
 
 			if (dist[next] > next_cost) {
 				dist[next] = next_cost;
-				pq.emplace(-dist[next], next);
+				pq.emplace(-next_cost, next);
 			}
 		}
 	}
@@ -43,19 +43,20 @@ bool dijkstra(int mid) {
 
 int main() {
 	scanf("%d %d %d", &N, &P, &K);
+	int maxV;
 	for (int i = 0; i < P; i++) {
 		int a, b, cost;
 		scanf("%d %d %d", &a, &b, &cost);
 		network[a].push_back({ b, cost });
 		network[b].push_back({ a, cost });
+		maxV = max(maxV, cost);
 	}
 
 	int answer = -1;
 
 	int minV = 0;
-	int maxV = 1000000;
 
-	while (minV < maxV) {
+	while (minV <= maxV) {
 		int mid = (maxV + minV) / 2;
 		if (dijkstra(mid)) { // N번까지 연결된 케이블들 중 가중치가 mid보다 크거나 같은 케이블의 개수가 K개 이하일 경우
 			answer = mid;
