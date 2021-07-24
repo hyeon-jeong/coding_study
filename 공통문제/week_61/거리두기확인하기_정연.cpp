3ba8b65889f090a88154ee06807826d345751322
jystella17 <42269221+jystella17@users.noreply.github.com>
2021-07-24 22:20:34 +0900

    Update 거리두기확인하기_정연.cpp

--- 공통문제/week_61/거리두기확인하기_정연.cpp
+++ 공통문제/week_61/거리두기확인하기_정연.cpp
@@ -13,42 +13,43 @@ vector<int> solution(vector<vector<string>> places) {
     vector<int> answer;
     
     for(int k=0; k<5; k++){ // 5개 대기실
-        queue<pair<int,int>> q; // 대기실마다 배치가 다르므로 k마다 초기화
+        queue<pair<pair<int,int>,int>> q;
         int distancing = 1;
         for(int i=0; i<5; i++){ // 대기실 당 5x5
             for(int j=0; j<5; j++){
                 visited[i][j] = false;
                 if(places[k][i][j] == 'P')
-                    q.push({i,j});
+                    q.push(make_pair(make_pair(i,j),0));
             }
         }
         
         while(!q.empty()){
-            int curx = q.front().first;
-            int cury = q.front().second;
+            int curx = q.front().first.first;
+            int cury = q.front().first.second;
+            int manhattan = q.front().second;
             q.pop();
             
-            int manhattan = 0;
+            if (manhattan == 2)
+                continue;
+            
             for(int i=0; i<4; i++){
                 int nextx = curx + dx[i];
                 int nexty = cury + dy[i];
                 
                 if(nextx>0 && nextx<5 && nexty>0 && nexty<5){
-                    if(visited[nextx][nexty] == false)
-                        visited[nextx][nexty] = true;
+                    if(visited[nextx][nexty] == false){
+                        if(places[k][nextx][nexty] == 'X'){
+                            continue;
+                        }
                     
-                    if(places[k][nextx][nexty] == 'X'){
-                        manhattan+2;
-                        continue;
-                    }
-                    
-                    else if(places[k][nextx][nexty] == 'O'){
-                        manhattan++;
-                    }
+                        else if(places[k][nextx][nexty] == 'O'){
+                            q.push(make_pair(make_pair(nextx,nexty), manhattan+1));
+                            visited[nextx][nexty] = true;
+                        }
 
-                    else if (places[k][nextx][nexty] == 'P'
-                            && manhattan <= 2)
-                        distancing = 0;
+                        else if (places[k][nextx][nexty] == 'P')
+                            distancing = 0;
+                    }
                 }
             }
         }
